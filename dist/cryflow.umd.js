1215b282468d337f544ea684942b7c74e5dec9d9
Randell Quitain <cprjk.buzz@gmail.com>
2020-02-27 21:31:02 +0800

    Updated build

---
+++ dist/cryflow.umd.js
@@ -0,0 +1,254 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.Cryflow = {}));
+}(this, (function (exports) { 'use strict';
+
+  //
+  //
+  //
+  //
+
+  var script = {
+    name: 'Cryflow',
+    props: {
+      chars: {
+        type: String,
+        default: '!<>-_\\/[]{}—=+*^?#________'
+      },
+      phrases: {
+        type: Array,
+        default: function () {
+          return []
+        }
+      }
+    },
+    data: function data () {
+      return {
+        el: null,
+        updatedText: null,
+        resolve: null,
+        queue: [],
+        frameRequest: null,
+        frame: null
+      }
+    },
+    methods: {
+      setText: function setText (newText) {
+        var this$1 = this;
+
+        var oldText = this.el.innerText;
+        var length = Math.max(oldText.length, newText.length);
+        var promise = new Promise(function (resolve) {
+          this$1.resolve = resolve;
+          return this$1.resolve
+        });
+        this.queue = [];
+        for (var i = 0; i < length; i++) {
+          var from = oldText[i] || '';
+          var to = newText[i] || '';
+          var start = Math.floor(Math.random() * 40);
+          var end = start + Math.floor(Math.random() * 40);
+          this.queue.push({ from: from, to: to, start: start, end: end });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.updateText();
+        return promise
+      },
+      updateText: function updateText () {
+        var output = '';
+        var complete = 0;
+        for (var i = 0, n = this.queue.length; i < n; i++) {
+          var ref = this.queue[i];
+          var from = ref.from;
+          var to = ref.to;
+          var start = ref.start;
+          var end = ref.end;
+          var char = ref.char;
+          if (this.frame >= end) {
+            complete++;
+            output += to;
+          } else if (this.frame >= start) {
+            if (!char || Math.random() < 0.28) {
+              char = this.randomChar();
+              this.queue[i].char = char;
+            }
+            output += "<span class=\"dud\">" + char + "</span>";
+          } else {
+            output += from;
+          }
+        }
+        this.el.innerHTML = output;
+        if (complete === this.queue.length) {
+          this.resolve();
+        } else {
+          this.frameRequest = requestAnimationFrame(this.updateText.bind(this));
+          this.frame++;
+        }
+      },
+      randomChar: function randomChar () {
+        return this.chars[Math.floor(Math.random() * this.chars.length)]
+      }
+    },
+    mounted: function mounted () {
+      var this$1 = this;
+
+      this.el = this.$el;
+      this.updatedText = this.updateText.bind(this);
+      var counter = 0;
+      var next = function () {
+        this$1.setText(this$1.phrases[counter]).then(function () {
+          setTimeout(next, 800);
+        });
+        counter = (counter + 1) % this$1.phrases.length;
+      };
+      next();
+    }
+  };
+
+  function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+  /* server only */
+  , shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+      createInjectorSSR = createInjector;
+      createInjector = shadowMode;
+      shadowMode = false;
+    } // Vue.extend constructor export interop.
+
+
+    var options = typeof script === 'function' ? script.options : script; // render functions
+
+    if (template && template.render) {
+      options.render = template.render;
+      options.staticRenderFns = template.staticRenderFns;
+      options._compiled = true; // functional template
+
+      if (isFunctionalTemplate) {
+        options.functional = true;
+      }
+    } // scopedId
+
+
+    if (scopeId) {
+      options._scopeId = scopeId;
+    }
+
+    var hook;
+
+    if (moduleIdentifier) {
+      // server build
+      hook = function hook(context) {
+        // 2.3 injection
+        context = context || // cached call
+        this.$vnode && this.$vnode.ssrContext || // stateful
+        this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+        // 2.2 with runInNewContext: true
+
+        if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+          context = __VUE_SSR_CONTEXT__;
+        } // inject component styles
+
+
+        if (style) {
+          style.call(this, createInjectorSSR(context));
+        } // register component module identifier for async chunk inference
+
+
+        if (context && context._registeredComponents) {
+          context._registeredComponents.add(moduleIdentifier);
+        }
+      }; // used by ssr in case component is cached and beforeCreate
+      // never gets called
+
+
+      options._ssrRegister = hook;
+    } else if (style) {
+      hook = shadowMode ? function () {
+        style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+      } : function (context) {
+        style.call(this, createInjector(context));
+      };
+    }
+
+    if (hook) {
+      if (options.functional) {
+        // register for functional component in vue file
+        var originalRender = options.render;
+
+        options.render = function renderWithStyleInjection(h, context) {
+          hook.call(context);
+          return originalRender(h, context);
+        };
+      } else {
+        // inject component registration as beforeCreate hook
+        var existing = options.beforeCreate;
+        options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+      }
+    }
+
+    return script;
+  }
+
+  var normalizeComponent_1 = normalizeComponent;
+
+  /* script */
+  var __vue_script__ = script;
+
+  /* template */
+  var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"cryflow"})};
+  var __vue_staticRenderFns__ = [];
+
+    /* style */
+    var __vue_inject_styles__ = undefined;
+    /* scoped */
+    var __vue_scope_id__ = undefined;
+    /* module identifier */
+    var __vue_module_identifier__ = undefined;
+    /* functional template */
+    var __vue_is_functional_template__ = false;
+    /* style inject */
+    
+    /* style inject SSR */
+    
+
+    
+    var component = normalizeComponent_1(
+      { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+      __vue_inject_styles__,
+      __vue_script__,
+      __vue_scope_id__,
+      __vue_is_functional_template__,
+      __vue_module_identifier__,
+      undefined,
+      undefined
+    );
+
+  function install(Vue) {
+    if (install.installed) { return }
+    install.installed = true;
+    Vue.component('Cryflow', component);
+  }
+
+  var plugin = {
+    install: install
+  };
+
+  var GlobalVue = null;
+  if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+  } else if (typeof global !== 'undefined') {
+    GlobalVue = global.vue;
+  }
+
+  if (GlobalVue) {
+    GlobalVue.use(plugin);
+  }
+
+  component.install = install;
+
+  exports.default = component;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
